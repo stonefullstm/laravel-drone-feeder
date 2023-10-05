d15ed57bca924b64a4d38a53207d5ff15a6edc67
stonefullstm <stonefull.stm@gmail.com>
2023-10-04 22:58:16 -0300

    Commit inicial - criação das migrations

---
+++ database/migrations/2023_10_05_013802_create_deliveries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->dateTime('data_entrega');
+            $table->string('status');
+            $table->bigInteger('drone_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('drone_id')
+                ->references('id')->on('drones')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
