f51a9890b3ea8be423c544a9df1bf21f46bf9d0c
stonefullstm <stonefull.stm@gmail.com>
2023-10-06 10:50:29 -0300

    feat: Adicionando validação para a rota POST /api/drones

--- app/Http/Controllers/DroneController.php
+++ app/Http/Controllers/DroneController.php
@@ -4,23 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Delivery;
 use App\Models\Drone;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class DroneController extends Controller
 {
 
     public function index() {
-        return Drone::all();
+        return Drone::with('deliveries:id,latitude,longitude,data_entrega,status,drone_id')
+            ->get(['id', 'name', 'model']);
     }
 
     public function store(Request $request) {
 
-        $drone = Drone::create([
-            'name'=>$request->input('name'),
-            'model'=>$request->input('model')
+        $validation = Validator::make($request->all(),[
+            'name' => 'required',
+            'model' => 'required',
         ]);
-    
-        return $drone;
+
+        if ($validation->fails()) {
+
+            $errors = $validation->errors();
+            return $errors->toJson();
+            
+        } else {
+            $drone = Drone::create([
+                'name'=>$request->input('name'),
+                'model'=>$request->input('model')
+            ]);
+        
+            return $drone;
+        }
     
     }
 
---
+++ app/Http/Requests/DroneStoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DroneStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'model' => 'required',
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required!',
+            'model.required' => 'Model is required!',
+        ];
+    }
+}
